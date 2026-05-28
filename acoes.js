2070fca36482fe566bfcb7d98fe249022c2a844d
NotFelBRo <tagm554@gmail.com>
2026-05-27 21:23:19 -0300

    correções na seleção do nav

--- acoes.js
+++ acoes.js
@@ -26,3 +26,30 @@ function enviarMensagem() {
     mensagem.value = "";
   }
 }
+
+function acessar() {
+  const email = document.querySelector('.campo input[type="email"]');
+  const senha = document.querySelector('.campo input[type="text"]');
+
+  let valido = true;
+
+  [email, senha].forEach((campo) => {
+    const aviso = campo.parentElement.querySelector(".aviso-erro");
+    if (aviso) aviso.remove();
+
+    if (campo.value.trim() === "") {
+      valido = false;
+
+      const span = document.createElement("span");
+      span.classList.add("aviso-erro");
+      span.textContent = "Preencha este campo";
+      campo.parentElement.appendChild(span);
+    }
+  });
+
+  if (valido) {
+    setTimeout(() => {
+      window.location.href = "./sistema.html";
+    }, 1000);
+  }
+}
